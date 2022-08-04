8afb263b4b2809c91a8b4bb519548f8f26bde1f4
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-08-05 01:29:03 +0500

    :pencil2: Add `__version__`

--- korrektor_py/__init__.py
+++ korrektor_py/__init__.py
@@ -1,6 +1,8 @@
 from .api import Client
 from .core import Korrektor
 
+__version__ = "1.0.0"
+
 __all__ = [
     "Client",
     "Korrektor"
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     from setuptools import setup
-    setup()
+    from korrektor_py import __version__
+
+    setup(version=__version__)

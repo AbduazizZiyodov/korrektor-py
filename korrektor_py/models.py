d28f1d5a74b0362eed61a829b22774ce456a0804
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-08-04 19:05:50 +0500

    :+1: Added response models, for serializing API response data to python obj.

--- korrektor_py/models.py
+++ korrektor_py/models.py
@@ -6,11 +6,28 @@ from pydantic import (
 
 
 class Text(BaseModel):
-    text: str
+    text: t.Optional[str]
+
+
+class Data(BaseModel):
+    data: t.Optional[t.Any]
 
 
 class Word(BaseModel):
-    word: str
+    word: t.Optional[str]
+
+
+class Response(BaseModel):
+    status: str
+    code: str
+
+
+class ResponseText(Response, Text):
+    ...
+
+
+class ResponseData(Response, Data):
+    ...
 
 
 class Alphabet(BaseModel):
@@ -19,13 +36,18 @@ class Alphabet(BaseModel):
     @validator("alphabet")
     def alphabet_match(cls, value: str):
         if value not in ["cyrillic", "latin"]:
-            raise ValueError("Value of alphabet should be cyrillic or latin")
+            raise ValueError(
+                "Value of alphabet should be cyrillic or latin"
+            )
         return value
 
 
 class SpellCheckData(BaseModel):
     words: t.List[str]
     remove_modifiers: t.Optional[bool] = False
+
+    class Config:
+        response_model = ResponseData
 
 
 class OcrData(BaseModel):
@@ -39,30 +61,48 @@ class DocData(Alphabet):
 class NumberToWordsData(BaseModel):
     num: int
 
+    class Config:
+        response_model = ResponseText
+
 
 class TransliterateData(Alphabet, Text):
-    ...
+    class Config:
+        response_model = ResponseText
 
 
 class AutoCorrectData(Text):
-    ...
+    class Config:
+        response_model = ResponseText
 
 
 class RemoveModifiersData(Text):
-    ...
+    class Config:
+        response_model = ResponseText
 
 
 class TokenizeData(Word):
-    ...
+    class Config:
+        response_model = ResponseText
 
 
 class WordFrequencyData(Text):
-    ...
+    class Config:
+        response_model = ResponseData
 
 
 class RemoveDuplicatesData(Text):
-    ...
+    class Config:
+        response_model = ResponseText
 
 
 class AlphabetSortingData(Text):
-    ...
+    class Config:
+        response_model = ResponseText
+
+
+__all__ = [
+    "SpellCheckData", "TransliterateData", "AutoCorrectData",
+    "RemoveModifiersData", "TokenizeData", "NumberToWordsData",
+    "WordFrequencyData", "RemoveDuplicatesData", "AlphabetSortingData",
+    "ResponseData", "ResponseText"
+]

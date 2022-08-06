b7dc46c5726984f34384b3f4e40fa2d19cdfd230
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-08-07 00:51:31 +0500

    :fire: Major improvements, create additional methods for sending image & document

--- korrektor_py/api.py
+++ korrektor_py/api.py
@@ -1,7 +1,19 @@
+import os
 import json
 import httpx
 import typing as t
 from pydantic import BaseModel
+
+from korrektor_py.models import (
+    Alphabet,
+    FileResponse,
+    ResponseText,
+    ResponseData,
+    ResponseError
+)
+
+from korrektor_py.exceptions import WrongFileFormat
+from korrektor_py.utils import check_file_format, get_file_info
 
 
 class Client:
@@ -16,41 +28,89 @@ class Client:
             "Authorization": f"Bearer {self.TOKEN}"
         })
         self.headers_json = httpx.Headers({
-            "Authorization": f"Bearer {self.TOKEN}"
+            "Authorization": f"Bearer {self.TOKEN}",
+            "Content-type": "application/json"
         })
 
-    def send(
+    def send_json(
             self,
             endpoint: str,
             Model: BaseModel,
             **kwargs
 
-    ) -> t.Union[httpx.Response, t.Any]:
-        """Main method for communicating with API
-        """
+    ) -> t.Union[ResponseText, ResponseData, ResponseError]:
         url = self.API_URL + endpoint
 
-        if endpoint not in ["ocr", "doc"]:
-            files = None
-            content: BaseModel = Model.parse_obj(kwargs)
-            content = json.dumps(content.dict(), indent=2)\
-                .encode('utf-8')
-            headers = self.headers_json
-        else:
-            content = None
-            files = Model.parse_obj(kwargs).dict()
-            headers = self.headers
+        request_dict: dict = Model.parse_obj(kwargs).dict()
+        data = json.dumps(request_dict, indent=2).encode('utf-8')
 
         response: httpx.Response = httpx.post(
             url,
-            timeout=60,  # seconds (if needs)
-            files=files,
-            headers=headers,
-            content=content,
+            timeout=60,
+            content=data,
+            headers=self.headers_json,
+        )
+        response_content = response.json()
+
+        if response_content["code"] != "200":
+            return ResponseError(**response_content)
+
+        return Model.Config.response_model.parse_obj(response_content)
+
+    def send_image(
+        self,
+        endpoint: str,
+        image_file_path: str
+    ) -> t.Union[ResponseText,  ResponseError]:
+        url = self.API_URL + endpoint
+
+        with open(image_file_path, "rb") as file:
+            image: bytes = file.read()
+
+        response: httpx.Response = httpx.post(
+            url,
+            timeout=60,
+            headers=self.headers,
+            files={"image": image},
         )
 
-        if response.json()["code"] == "200":
-            return Model.Config\
-                .response_model.parse_obj(response.json())
+        response_content: dict = response.json()
 
-        return response.json()
+        if response_content["code"] != "200":
+            return ResponseError(**response_content)
+
+        return ResponseText(**response_content)
+
+    def send_document(
+        self,
+        endpoint: str,
+        document_path: str,
+        alphabet: str,
+        save_path: t.Optional[str] = None,
+    ) -> t.Union[ResponseError, FileResponse]:
+        url = self.API_URL + endpoint
+
+        alphabet: BaseModel = Alphabet(alphabet=alphabet)
+        file_name, file_format = get_file_info(document_path)
+
+        if save_path is None:
+            save_path = f"korrektor.uz-{file_name}.{file_format}"
+
+        response: httpx.Response = httpx.post(
+            url,
+            timeout=60,
+            headers=self.headers,
+            data=alphabet.dict(),
+            files={"doc": open(document_path, "rb")}
+        )
+
+        if response.headers["content-type"].startswith("application/json"):
+            return ResponseError.parse_obj(response.json())
+
+        with open(save_path, "wb") as file:
+            file.write(response.content)
+
+        return FileResponse(
+            file_name=save_path,
+            status="File saved."
+        )
